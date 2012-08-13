4e68beed3cd684d44ab28e93653c01d52908db88
Vic <vic@kiwi.la>
2012-08-13 18:05:49 +0300

    Update content/heads.php

--- content/heads.php
+++ content/heads.php
@@ -32,7 +32,7 @@ $count = $list['playerCount'];
 if($count != 0) {
 	// Display faces
 	foreach ($playerlist as $value) {
-		echo '<img name="'.$value.'" style="-webkit-border-radius: 1.9px; -moz-border-radius: 1.9px; border-radius: 1.9px; border: 1px solid black; margin-right: 3px; margin-bottom: 2px;" height="32px" title="'.$value.'" alt="'.$value.'" src="'.$avatar.'?p='.$value.'&size=32" />';
+		echo '<img name="'.$value.'" style="-webkit-border-radius: 1.9px; -moz-border-radius: 1.9px; border-radius: 1.9px; border: 1px solid black; margin-right: 3px; margin-bottom: 2px;" height="32px" width="32px" title="'.$value.'" alt="'.$value.'" src="'.$avatar.'?p='.$value.'&size=32" />';
 	}
 }
 else {
